2ac2afd63ef5db4ec1fb8bc74fe7a347d0922183
Silvan Wegmann <silvan.wegmann@hslu.ch>
2021-12-06 15:09:51 +0100

    Move all widgets to dedicated file

--- pyquiz5.py
+++ pyquiz5.py
@@ -3,7 +3,7 @@ import pygame.gfxdraw
 import copy
 import random
 import style
-from label import *
+from widgets import *
 
 
 class Game:
@@ -64,50 +64,6 @@ pygame.mixer.init()
 hit = pygame.mixer.Sound("sounds/hit.wav")
 screen = pygame.display.set_mode((600, 400))
 clock = pygame.time.Clock()
-
-
-class Button(pygame.sprite.Sprite):
-    """A pygame GUI button."""
-    def __init__(self, position, text,
-        theme=style.pyquiz_theme,
-        command=None):
-        super().__init__()
-
-        self.text = text
-        self.style = copy.copy(theme.button)
-        self.command = command
-
-        self.set_text(self.text)
-        self.rect = self.image.get_rect()
-        self.rect.x, self.rect.y = position
-        self.rect.w = 500
-
-    def set_text(self, text):
-        self.image = self.style.font.render(text, 1, self.style.normal.text_color)
-
-    def update(self, screen):
-        colors = self._current_gui_colors()
-        self._draw_button(screen, colors)
-
-    def handle_event(self, event):
-        if event.type == pygame.MOUSEBUTTONUP:
-            if self.rect.collidepoint(pygame.mouse.get_pos()):
-                if self.command is not None:
-                    self.command()
-
-    def _current_gui_colors(self):
-        if self.command is None:
-            return self.style.normal
-        if self.rect.collidepoint(pygame.mouse.get_pos()):
-            return self.style.hover
-        return self.style.normal
-
-    def _draw_button(self, screen, colors):
-        # the width is set to 500 to have the same size not depending on the text size
-        pygame.draw.rect(screen, colors.background_color,
-            (self.rect.x - 50, self.rect.y, 500 , self.rect.h))
-        pygame.gfxdraw.rectangle(screen, (self.rect.x - 50, self.rect.y, 500 , self.rect.h),
-            self.style.border_color)
 
 
 class Ui:
--- widgets.py
+++ widgets.py
@@ -1,51 +1,70 @@
+import copy
 import pygame
-import random
+import style
 
 
-pygame.init()
-font = pygame.font.SysFont("Arial", 20)
-class Label:
-	''' CLASS FOR TEXT LABELS ON THE WIN SCREEN SURFACE '''
-	def __init__(self, screen, text, x, y, color="white"):
-		self.image = font.render(text, 1, color)
-		_, _, w, h = self.image.get_rect()
-		self.rect = pygame.Rect(x, y, w, h)
-		self.screen = screen
+class Label(pygame.sprite.Sprite):
+	"""A pygame GUI label."""
+	def __init__(self, position, text, theme=style.pyquiz_theme, style=style.Style()):
+		super().__init__()
 
-	def change_text(self, newtext):
-		self.image = font.render(newtext, 1, "white")
+		self.style = copy.copy(theme.label)
+		self.style.update(style)
+
+		self.set_text(text)
+		self.rect = self.image.get_rect()
+		self.rect.x, self.rect.y = position
+
+	def set_text_color(self, color):
+		self.style.text_color = color
+
+	def set_text(self, newtext):
+		self.image = self.style.font.render(newtext, 1, self.style.text_color)
 
 	def draw(self):
 		self.screen.blit(self.image, (self.rect))
 
 
+class Button(pygame.sprite.Sprite):
+    """A pygame GUI button."""
+    def __init__(self, position, text,
+        theme=style.pyquiz_theme,
+        command=None):
+        super().__init__()
 
+        self.text = text
+        self.style = copy.copy(theme.button)
+        self.command = command
 
-'''     when you import this module
-text1 = Text(win, "Ciao a tutti", 100, 100) # out of loop
-text.draw() # into the loop
-'''
+        self.set_text(self.text)
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = position
+        self.rect.w = 500
 
-if __name__ == '__main__':
-	# TEXT TO SHOW ON THE SCREEN AT POS 100 100
-	win = pygame.display.set_mode((600, 600))
-	clock = pygame.time.Clock()
-	score = Label(win, "Ciao a tutti", 100, 100)
+    def set_text(self, text):
+        self.image = self.style.font.render(text, 1, self.style.normal.text_color)
 
-	# LOOP TO MAKE THINGS ON THE SCRREEN
-	loop = 1
-	while loop:
-		win.fill(0) # CLEAN THE SCREEN EVERY FRAME
-		# CODE TO CLOSE THE WINDOW
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				loop = 0
-			if event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_ESCAPE:
-					loop = 0
-		# CODE TO SHOW TEXT EVERY FRAME
-		score.draw()
-		pygame.display.update()
-		clock.tick(60)
+    def update(self, screen):
+        colors = self._current_gui_colors()
+        self._draw_button(screen, colors)
 
-	pygame.quit()
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONUP:
+            if self.rect.collidepoint(pygame.mouse.get_pos()):
+                if self.command is not None:
+                    self.command()
+
+    def _current_gui_colors(self):
+        if self.command is None:
+            return self.style.normal
+        if self.rect.collidepoint(pygame.mouse.get_pos()):
+            return self.style.hover
+        return self.style.normal
+
+    def _draw_button(self, screen, colors):
+        # the width is set to 500 to have the same size not depending on the text size
+        pygame.draw.rect(screen, colors.background_color,
+            (self.rect.x - 50, self.rect.y, 500 , self.rect.h))
+        pygame.gfxdraw.rectangle(screen, (self.rect.x - 50, self.rect.y, 500 , self.rect.h),
+            self.style.border_color)
+			
